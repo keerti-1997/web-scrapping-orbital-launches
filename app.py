efb9980e171d906027640ea95b5f7e643438556d
keerti-1997 <bhogarajukeerti17@gmail.com>
2020-01-10 11:37:22 -0800

    Beautified code with comments and removed print statements v2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# import libraries
 import requests
 import csv
 from bs4 import BeautifulSoup
@@ -8,6 +9,7 @@ import numpy as np
 
 months = [name for name in list(calendar.month_name) if name != '']
 
+# download the webpage
 page = requests.get("https://en.wikipedia.org/wiki/2019_in_spaceflight#Orbital_launches")
 
 # BS object
@@ -16,6 +18,7 @@ soup = BeautifulSoup(page.text, 'html.parser')
 # extract the table under Orbital Launches 
 table = soup.find_all("table",{"class":"wikitable collapsible"})[0] 
 
+# get all the rows
 table_rows = table.find_all('tr')
 
 # All rows in one list
@@ -24,42 +27,41 @@ for tr in table_rows:
 	td = tr.find_all('td') 
 	row = [i.text.replace("\n","") for i in td] 
 	all_tds.append(row) 
-	print(row)
+	#print(row)
 
-# Ignoring the heading
+# Ignoring the table headings
 all_tds = all_tds[3:]
 
-# payloads dataframe
+# payloads 
 all_payloads = []
-#
 
-isMonth = False 
-isLaunch = False 
-isPayload = False 
-num_launches = 0 
-num_payloads = 0
-currMonth = "NA" 
-curr_date = ""
-rocket_name = ""
+
+isMonth = False 	 								# is the row displaying month or payload/launch information
+isLaunch = False 									# is the row displaying launch information
+isPayload = False 									# is the row displaying payload information 
+num_launches = 0 									# total number of launches
+num_payloads = 0									# total number of payloads
+currMonth = "NA" 									# launch month
+curr_date = ""										# launch date	
+rocket_name = ""									# name of the rocket associated with the payload
 
 for td in all_tds:
 	try: 
-		if len(td) == 1 or len(td) == 2: 
+		if len(td) == 1 or len(td) == 2: 			# to extract month
 			for s in td: 
 				idx1 = s.find("edit") 
 				idx2 = s.find(chr(8594)) 
 				if idx1 != -1: 
 					m = s[0:idx1-1] 
 					if m in months: 
-						print("YES") 
 						isMonth = True 
 						currMonth = m 
 					if idx2 != -1: 
 						m = s[s.find(chr(8594))+1:idx1-1] 
 						if m in months: 
-							print("YES") 
 							isMonth = True 
 							currMonth = m 
+
 		else: 
 			isMonth = False 
 			#print("Data ",td,len(td),isMonth,currMonth) 
@@ -74,13 +76,12 @@ for td in all_tds:
 				day = int(launch_date.split(" ")[0])
 				month = -1
 
-				hourse = 0
+				hours = 0
 				minutes = 0
 				seconds = 0 
 				mstr = ""
 				for m in months: 
-					if m in launch_date: 
-						print(months.index(m)+1) 
+					if m in launch_date:  
 						month = months.index(m)+1 
 						mstr = m
 
@@ -99,39 +100,45 @@ for td in all_tds:
 					else:
 						seconds = int(time.split(":")[2])
 				
+				# launch date in ISO format
 				curr_date = datetime(year, month, day, hours, minutes, seconds, tzinfo=timezone.utc).isoformat()
-				print("Launch ",td,len(td),isMonth,isLaunch,currMonth,curr_date) 
 				num_launches += 1 
-				#print("launches") 
+				
 			
 			if len(td) == 6: 
 				isPayload = True 
 				outcome = td[5]
 				payload_name = td[0]
-				print("Payload ",td,len(td),isMonth,isPayload,currMonth,curr_date,rocket_name,outcome) 
+				
+				# includes rocket names of whose atleast one payload's outcome is either Successful, Operational or En route
 				if outcome in ["Successful","Operational","En route"]:
 					payload = [curr_date,rocket_name,payload_name,outcome]
 					all_payloads.append(payload)
 				num_payloads += 1 
-				#print("payloads") 
-	except Exception as e:
-		print(e," ",td)		
 
+	except Exception as e:
+
+		pass		
+
+# DataFrame of payloads and rocket names
 all_payloads = pd.DataFrame(all_payloads,columns=["LaunchDate","RocketName","PayloadName","Outcome"])
 all_payloads["Date"] = all_payloads.apply(lambda row: row["LaunchDate"][0:10],axis=1)
-
+# Get distinct count of number of launches per day
 final_count = pd.DataFrame(all_payloads["RocketName"].groupby(all_payloads["Date"]).unique()).reset_index()
 final_count["Num_Launches"] = final_count.apply(lambda row: len(row["RocketName"]),axis=1) 
 final_count["Date"] = pd.to_datetime(final_count["Date"])
 
+# Initialise dataframe with all dates of the year 2019 and number of launches equal to zero
 date_range = pd.Series(pd.date_range(start ='1-1-2019',end ='31-12-2019'))
 date_range = pd.concat([date_range,pd.Series(np.zeros(dtype="int",shape=len(date_range)))],axis=1)
 date_range.columns = ["LDate","Num_Launches"]
 date_range["LaunchDate"] = pd.Series([x.isoformat() for x in date_range["LDate"]])
 
+# Merge final_count with date_range (Left Outer Join) which gives all the dates and number of launches per date 
 df = date_range.merge(final_count[["Date","Num_Launches"]],left_on="LDate",right_on="Date",how="left")
-df["Num_Launches"] = df["Num_Launches_y"].fillna(0)
+df["Num_Launches"] = df["Num_Launches_y"].fillna(0).astype(int)
 df = df[["LaunchDate","Num_Launches"]] 
 
+# Save the dataframe to csv
 df.columns = ["date","value"]
 df.to_csv("output.csv",index=False) 
